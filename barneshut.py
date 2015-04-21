65b40559eb605ba4bfdb972d52db6d051c5ca61a
AlexRothwell7 <alexrothwell7@gmail.com>
2015-04-21 15:36:52 +1000

    Finish initial implementation of BHGSA

--- barneshut.py
+++ barneshut.py
@@ -11,21 +11,45 @@ class BHNTree(object):
         self.dims = dims
         
     def insert(self, sol):
+        stack = [sol]
+        trees = [self]
+        
+        while stack:
+            curr = stack.pop()
+            tree = trees.pop()
+            children = tree.children
+            if tree.body is None:
+                tree.body = curr
+            elif not tree.isLeaf():
+                tree.body = getCentreMass(tree.body, curr)
+                child_num = tree.getChildNum(curr)
+                if child_num not in children:
+                    children[child_num] = BHNTree(tree.theta, getMids(tree, child_num), tree.side/2, tree.dims)
+                trees.push(children[child_num])
+                stack.push(curr)
+            else:
+                if tree.side / np.linalg.norm(tree.body.pos-curr.pos) < 1 << 5:
+                    child_num = tree.getChildNum(tree.body)
+                    children[child_num] = BHNTree(tree.theta, getMids(tree, child_num), tree.side/2, tree.dims)
+                    trees.push(children[child_num])
+                    stack.push(tree.body)
+                    child_num = tree.getChildNum(curr)
+                    if child_num not in children:
+                        children[child_num] = BHNTree(tree.theta, getMids(tree, child_num), tree.side/2, tree.dims)
+                    trees.push(children[child_num])
+                    stack.push(curr)
+                tree.body = getCentreMass(curr, tree.body)
         
     def getForce(self, sol):
         force = np.zeros(self.dims)
         if self.isLeaf():
             if this.body is not None and this.body is not sol:
                 force += addForce(this.body, sol)
-            elif self.side / np.linalg.norm(sol.pos-self.body.pos) < self.theta:
-                force += addForce(this.body, sol)
-            else:
-            {
-                for (BHTree sub : subtrees.values())
-                {
-                    sub.updateForce(s,max);
-                }
-            }
+        elif self.side / np.linalg.norm(sol.pos-self.body.pos) < self.theta:
+            force += addForce(this.body, sol)
+        else:
+            for child in self.children.values():
+                force += child.getForce(sol)
     
     def isLeaf(self):
         return not self.children
@@ -45,30 +69,36 @@ class BHNTree(object):
 
 
 def bhForce(func, pop, kbest, G):
-    side = getBounds(pop, func.dims, 1e-15)
+    lower, upper = getBounds(kbest, func.dims, 1e-15)
+    range = upper - lower
+    mids = np.array([upper - range/2 for i in range(func.dims)])
     
-    tree = BHNTree()
-    for sol in pop:
-        if sol in kbest:
-            tree.insert(sol)
+    tree = BHNTree(theta, mids, range, func.dims)
+    if sol in kbest:
+        tree.insert(sol)
     
     for sol in pop:
         sol.force = tree.getForce(sol) * G
+    
     Box box = new Box(mids,range);
     BHTree tree = new BHTree(box,dims,prob,theta);
     
     
 def getBounds(pop, dims, eps):
-    mids
-    upper = np.array(pop[0].pos)
-    lower = np.array(pop[0].pos)
+    upper = 0
+    lower = 0
     for sol in pop:
         for i in range(dims):
-            if sol.pos[i] > upper[i]:
-                upper[i] = sol.pos[i] + eps
-            elif sol.pos[i] < lower[i]:
-                lower[i] = sol.pos[i] - eps
+            if sol.pos[i] > upper:
+                upper = sol.pos[i] + eps
+            elif sol.pos[i] < lower:
+                lower = sol.pos[i] - eps
     return lower, upper
+    
+def getMids(tree, num):
+
+def getCentreMass(body1, body2):
+    
 
 if __name__ == '__main__':
     main()
