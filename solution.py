6559e78ba721ce19f158f1fcb06ffca8b1878c54
AlexRothwell7 <alexrothwell7@gmail.com>
2015-05-03 15:54:57 +1000

    Continue implementation of bhtree

--- solution.py
+++ solution.py
@@ -6,6 +6,7 @@ class Solution(object):
         self.force = np.array([0] * dims, dtype=float)
         self.vel = np.array(self.force, copy=True, dtype=float)
         self.mass = mass
+        self.dims = dims
 
     def __repr__(self):
         return ",".join(self.pos.tolist())
